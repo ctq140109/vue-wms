ee48301d7998bfd2ec7dc83f5f56f523d3f81ee3
ctq140109 <1401095999@qq.com>
2019-08-27 01:20:51 +0800

    拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,4 @@
 import request from '../utils/request';
-import qs from 'Qs';
-const headers = {
-    www: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
-    json: { 'Content-Type': 'application/json;charset=UTF-8' },
-    form: { 'Content-Type': 'multipart/form-data' }
-};
-// export const fetchData = (query) => {
-//     return request({
-//         url: '/ms/table/list',
-//         method: 'post',
-//         data: query
-//     })
-// }
 //获取企业角色
 export const getRoles = (query) => {
     return request({
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,18 +2,15 @@ import axios from 'axios';
 import qs from 'Qs';
 import { Message } from 'element-ui';
 const headers = {
-    www: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
-    json: { 'Content-Type': 'application/json;charset=UTF-8' },
-    form: { 'Content-Type': 'multipart/form-data' }
+    www: 'application/x-www-form-urlencoded;charset=UTF-8',
+    json: 'application/json;charset=UTF-8',
+    form: 'multipart/form-data'
 };
 const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // baseURL: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
     // baseURL: 'https://www.apollo-wms.com',
     baseURL: 'http://localhost:16443',
-    // headers:{'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'},
-    // headers:{'Content-Type':'application/json;charset=UTF-8'},
-    // headers:{'Content-Type':'multipart/form-data'},
     timeout: 5000
 })
 
@@ -21,13 +18,13 @@ service.interceptors.request.use(config => {
     console.log(config);
     //拦截设置请求头
     switch (config.type) {
-        case 'json': config.headers["Content-Type"] = 'application/json;charset=UTF-8'; break;
-        case 'www': 
-            config.headers["Content-Type"] = 'application/x-www-form-urlencoded;charset=UTF-8';
+        case 'json': config.headers["Content-Type"] = headers.json; break;
+        case 'www':
+            config.headers["Content-Type"] = headers.www;
             config.data = qs.stringify(config.data);
             break;
-        case 'form': config.headers["Content-Type"] = 'multipart/form-data'; break;
-        default:break;
+        case 'form': config.headers["Content-Type"] = headers.form; break;
+        default: break;
     }
     return config;
 }, error => {
@@ -37,14 +34,16 @@ service.interceptors.request.use(config => {
 
 service.interceptors.response.use(response => {
     if (response.status === 200) {
+        console.log('请求成功');
         // response.data数据结构{meta:{},data:''}
         if (response.data.meta.success == false) {//拦截判断
+            console.log(response.data.meta.code);
             switch (response.data.meta.code) {
-                case 1000://登录过期
+                case '1000'://登录过期
 
                     break;
-                case 600://登录错误信息
-                    this.$message.error(response.data.meta.message);
+                case '600'://登录错误信息
+                    Message.warning(response.data.meta.message);
                     break;
                 default: break;
             }
@@ -54,29 +53,19 @@ service.interceptors.response.use(response => {
         }
     } else {
         console.log('服务器繁忙请稍后重试！');
-        switch (response.status) {//http状态码拦截提示
-            case 403:
-                break;
-            case 404:
-                // this.message.error('链接找不到了！');
-                // this.$message.error(response.statusText + '链接找不到了！' + response.url);
-                break;
-            case 500:
-                console.log('服务器繁忙请稍后重试！');
-                // this.$message.error(response.statusText + '服务器繁忙请稍后重试！' + response.url);
-                break;
-            case 504:
-                // this.$message.error(response.statusText + '网络连接超时！请稍后重试！' + response.url);
-                break;
-            default:
-                // this.$message.error(response.statusText + response.url);
-                break;
-        }
+        Message.error('服务器繁忙请稍后重试！');
         Promise.reject();
     }
 }, error => {
-    console.log(error);
-    Message.error('链接找不到了！');
+    console.log(error.response);
+    switch (error.response.status) {//http状态码拦截提示
+        case 404:
+            Message.error('链接找不到了！');
+            break;
+        default:
+            Message.error('服务器繁忙请稍后重试！');
+            break;
+    }
     return Promise.reject();
 })
 
